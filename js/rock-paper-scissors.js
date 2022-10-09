da95316b00d03b50c4d3f1d1ff9013ec97a14ade
Alex Nico <a.nicolaidis@outlook.com>
2022-10-09 12:42:38 +1100

    MVP completed of R,P,S game

--- js/rock-paper-scissors.js
+++ js/rock-paper-scissors.js
@@ -1,4 +1,45 @@
-//Script for user to select and options and play R,P,S against the computer
+$(document).ready(function(){
+    const choices = ['rock', 'paper','scissors'];
 
-//computer to select a random choice
+    const computerSelects = function (array){
+        let arrayIndex = Math.floor(Math.random()*array.length)
+        return array[arrayIndex];
+    }
 
+    const playerSelects = function (){
+        const playerSelection = $(this).children().attr('class');
+        const computerSelection = computerSelects(choices);
+        let winner = '';
+        if(playerSelection === computerSelection){
+            winner = `The game was a tie both you and the computer chose ${playerSelection}.`
+        }else if (playerSelection === 'rock'){
+            if(computerSelection === 'paper'){
+                winner = `The computer wins, you chose ${playerSelection} and the computer chose ${computerSelection}.`
+            }else{
+                winner =  `You win, you chose ${playerSelection} and the computer chose ${computerSelection}.`
+            }
+        }else if(playerSelection === 'paper'){
+            if (computerSelection === 'rock'){
+                return "You win"
+            }else{
+                winner = `The computer wins, you chose ${playerSelection} and the computer chose ${computerSelection}`
+            }
+        }else{
+            if (computerSelection === 'rock'){
+                winner = `The computer wins, you chose ${playerSelection} and the computer chose ${computerSelection}`
+            }else{
+                winner =  `You win, you chose ${playerSelection} and the computer chose ${computerSelection}.`
+            }
+        }
+        
+        $('.result-text').html(winner);
+        $('div.result-screen').addClass('visibility');
+    }
+
+    const refresh = function(){
+        $('div.result-screen').removeClass('visibility');
+    }
+
+    $('.button').on ('click', playerSelects);
+    $('button').on('click', refresh)
+});
